f4785e53486933ac9b6c01f50e0bfc3373597817
smac4343 <alexmarksonit@gmail.com>
2025-07-19 20:21:54 +0300

    refactor api

--- src/scripts/api/api.js
+++ src/scripts/api/api.js
@@ -1,86 +1,66 @@
-const config = {
-  baseUrl: 'https://nomoreparties.co/v1/wff-cohort-27',
-  headers: {
-    authorization: '78d647ac-c117-4622-a9f8-fa9f9240e132',
-    'Content-Type': 'application/json'
-  }
-};
-
-const checkResponse = (res) => {
-  if (res.ok) {
-    return res.json();
-  }
-  return Promise.reject(`Ошибка: ${res.status}`);
-};
+import { config } from "./config";
+import { checkResponse } from "./utils";
 
 export const getUserInfo = () => {
   return fetch(`${config.baseUrl}/users/me`, {
-    headers: config.headers
-  })
-    .then(checkResponse);
+    headers: config.headers,
+  }).then(checkResponse);
 };
 
 export const editProfile = ({ name, about }) => {
   return fetch(`${config.baseUrl}/users/me`, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: config.headers,
     body: JSON.stringify({
       name,
-      about
-    })
-  })
-    .then(checkResponse);
+      about,
+    }),
+  }).then(checkResponse);
 };
 
 export const updateAvatar = (avatar) => {
   return fetch(`${config.baseUrl}/users/me/avatar`, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: config.headers,
-    body: JSON.stringify({ avatar })
-  })
-    .then(checkResponse);
+    body: JSON.stringify({ avatar }),
+  }).then(checkResponse);
 };
 
 // Карточки
 export const getInitialCards = () => {
   return fetch(`${config.baseUrl}/cards`, {
-    headers: config.headers
-  })
-    .then(checkResponse);
+    headers: config.headers,
+  }).then(checkResponse);
 };
 
 export const addCard = ({ name, link }) => {
   return fetch(`${config.baseUrl}/cards`, {
-    method: 'POST',
+    method: "POST",
     headers: config.headers,
     body: JSON.stringify({
       name,
-      link
-    })
-  })
-    .then(checkResponse);
+      link,
+    }),
+  }).then(checkResponse);
 };
 
 export const deleteCard = (cardId) => {
   return fetch(`${config.baseUrl}/cards/${cardId}`, {
-    method: 'DELETE',
-    headers: config.headers
-  })
-    .then(checkResponse);
+    method: "DELETE",
+    headers: config.headers,
+  }).then(checkResponse);
 };
 
 export const likeCard = (cardId) => {
   return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-    method: 'PUT',
-    headers: config.headers
-  })
-    .then(checkResponse);
+    method: "PUT",
+    headers: config.headers,
+  }).then(checkResponse);
 };
 
 export const dislikeCard = (cardId) => {
   return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-    method: 'DELETE',
-    headers: config.headers
-  })
-    .then(checkResponse);
+    method: "DELETE",
+    headers: config.headers,
+  }).then(checkResponse);
 };
---
+++ src/scripts/api/config.js
@@ -0,0 +1,7 @@
+export const config = {
+  baseUrl: "https://nomoreparties.co/v1/wff-cohort-36",
+  headers: {
+    authorization: "d187beff-631e-4e8e-9aa7-54a801cf35e5",
+    "Content-Type": "application/json",
+  },
+};
